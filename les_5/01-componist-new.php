945f4edb6341c4a69a14bc8106df42b2345c441d
NoutDechering <NB.dechering@student.han.nl>
2024-12-19 10:08:23 +0100

    Create 01-componist-new.php

---
+++ les_5/01-componist-new.php
@@ -0,0 +1,20 @@
+<?php
+
+$melding = 'Gegevens worden nog niet verwerkt';
+
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Componinst - nieuw</title>
+    <link href="css/normalize.css" rel="stylesheet" >
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+    <?= $melding ?>
+</body>
+</html>
